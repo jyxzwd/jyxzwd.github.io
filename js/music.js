2dd570c2f334064f340ac88074dd36ad5ed1a517
jyxzwd <978344408@qq.com>
2024-09-18 06:35:54 +0000

    Site updated: 2024-09-18 06:35:52

---
+++ js/music.js
@@ -0,0 +1,15 @@
+// function toggleSound() {
+//     var music = document.getElementById("player");//获取ID
+//
+//     if (music.paused) { //判读是否播放
+//         music.paused = false;
+//         music.play(); //没有就播放
+//     }
+// }
+//
+// window.onload = function () {
+//     let bodyElement = document.getElementsByTagName('body')[0];
+//     bodyElement.onclick = function () {
+//         toggleSound()
+//     }
+// }
